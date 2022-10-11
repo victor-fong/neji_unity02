9249c59786fc2e934841c5ce0db88b0f8962157b
Victor Fong <v.fong@dell.com>
2022-10-11 14:12:52 -0400

    Added initial work

---
+++ Assets/MagicLeap/Examples/Scripts/Visualizers/RawVideoCaptureVisualizer.cs
@@ -0,0 +1,129 @@
+// %BANNER_BEGIN%
+// ---------------------------------------------------------------------
+// %COPYRIGHT_BEGIN%
+//
+// Copyright (c) 2019-present, Magic Leap, Inc. All Rights Reserved.
+// Use of this file is governed by the Developer Agreement, located
+// here: https://auth.magicleap.com/terms/developer
+//
+// %COPYRIGHT_END%
+// ---------------------------------------------------------------------
+// %BANNER_END%
+
+using System;
+using System.Collections;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.XR.MagicLeap;
+
+namespace MagicLeap
+{
+    /// <summary>
+    /// This class handles visualization of the video and the UI with the status
+    /// of the recording.
+    /// </summary>
+    public class RawVideoCaptureVisualizer : MonoBehaviour
+    {
+        [SerializeField]
+        private WebClient _webClient;
+
+        private bool _ready;
+
+        private int _compressionLevel = 1;
+
+        /// <summary>
+        /// Check for all required variables to be initialized.
+        /// </summary>
+        void Start()
+        {
+            _ready = false;
+            Debug.Log("Initializing Raw Video Capture...");
+            if (_webClient == null) {
+                _webClient = GameObject.Find("WebClient").GetComponent<WebClient>();
+            }
+        }
+
+        /// <summary>
+        /// Handles video capture being started.
+        /// </summary>
+        public void OnCaptureStarted()
+        {
+            Debug.Log("Capture Starts...");
+            
+            if (_webClient == null) {
+                Debug.LogError("WebClient Not Attached!");
+            }
+            _webClient.StartClient();
+            _ready = true;
+        }
+
+        /// <summary>
+        /// Handles video capture ending.
+        /// </summary>
+        public void OnCaptureEnded()
+        {
+            Debug.Log("Capture Ends...");
+            _webClient.Close();
+        }
+
+        #if PLATFORM_LUMIN
+        /// <summary>
+        /// Display the raw video frame on the texture object.
+        /// </summary>
+        /// <param name="extras">Unused.</param>
+        /// <param name="frameData">Contains raw frame bytes to manipulate.</param>
+        /// <param name="frameMetadata">Unused.</param>
+        public void OnRawCaptureDataReceived(MLCamera.ResultExtras extras, MLCamera.YUVFrameInfo frameData, MLCamera.FrameMetadata frameMetadata)
+        {   // createYTexture(frameData);
+            // createUVTexture(frameData);
+            if (_ready) {
+                sendFrame(frameData);
+            }
+        }
+        #endif
+
+        /// <summary>
+        /// Disables the rendere.
+        /// </summary>
+        public void OnRawCaptureEnded()
+        {
+            Debug.Log("Ended capture data");
+        }
+
+        private async void sendFrame(MLCamera.YUVFrameInfo frameData) {
+            await Task.Run(() => {
+                YUV2RGB(frameData);
+            });
+        }
+
+        private void YUV2RGB(MLCamera.YUVFrameInfo frameData) {
+            
+            // Debug.Log(String.Format("Y | Width: {0} | Height: {1} | Stride {2} | Data: {3}", frameData.Y.Width, frameData.Y.Height, frameData.Y.Stride, frameData.Y.Data.Length));
+            // Debug.Log(String.Format("U | Width: {0} | Height: {1} | Stride {2} | Data: {3}", frameData.U.Width, frameData.U.Height, frameData.U.Stride, frameData.U.Data.Length));
+            // Debug.Log(String.Format("V | Width: {0} | Height: {1} | Stride {2} | Data: {3}", frameData.V.Width, frameData.V.Height, frameData.V.Stride, frameData.V.Data.Length));
+            _ready = false;
+            int bufferSize = (frameData.Y.Data.Length + frameData.U.Data.Length + frameData.V.Data.Length) / this._compressionLevel;
+            byte[] newBuffer = new byte[bufferSize];
+            int i = 0;
+            for (int j=0; j < frameData.Y.Data.Length; j++) {
+                if ((j % this._compressionLevel) == 0) {
+                    newBuffer[i++] = frameData.Y.Data[j];
+                }
+            }
+
+            for (int j=0; j < frameData.U.Data.Length; j++) {
+                if ((j % this._compressionLevel) == 0) {
+                    newBuffer[i++] = frameData.U.Data[j];
+                }
+            }
+
+            for (int j=0; j < frameData.V.Data.Length; j++) {
+                if ((j % this._compressionLevel) == 0) {
+                    newBuffer[i++] = frameData.V.Data[j];
+                }
+            }
+            _webClient.Send(newBuffer);
+            _ready = true;
+        }
+    }
+}
---
+++ Assets/WebClient.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class WebClient : MonoBehaviour
+{
+    
+    private Socket _sender;
+
+    void Start()
+    {
+        
+    }
+
+    void OnDestroy() {
+        _sender.Shutdown(SocketShutdown.Both);
+        _sender.Close();
+    }
+
+    public void Close() {
+        _sender.Shutdown(SocketShutdown.Both);
+        _sender.Close();
+    }
+
+    private void _send(byte[] data) {
+        Debug.Log("Sending...");
+        byte[] lengthBytes = BitConverter.GetBytes(data.Length);
+        Debug.Log(String.Format("Sending {0} bytes", data.Length));
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(lengthBytes);
+        _sender.Send(lengthBytes);
+
+        int totalSent = 0; 
+        int sent = -1;
+        while((sent = _sender.Send(data, totalSent, data.Length - totalSent, SocketFlags.None)) > 0)
+        {
+            totalSent += sent;
+        }
+        Debug.Log("Finished sending...");
+    }
+
+    public void Send(byte[] data){
+        _send(data);
+    }
+
+    public void StartClient() {
+        try
+        {
+            // Connect to a Remote server
+            // Get Host IP Address that is used to establish a connection
+            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+            // If a host has multiple addresses, you will get a list of addresses
+
+            // IPHostEntry host = Dns.GetHostEntry("localhost");
+            // IPAddress ipAddress = host.AddressList[0];
+            IPAddress ipAddress = IPAddress.Parse("192.168.86.42");
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8989);
+
+            // Create a TCP/IP  socket.
+            _sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try
+            {
+                // Connect to Remote EndPoint
+                _sender.Connect(remoteEP);
+                Debug.Log("WebClient Ready...");
+            }
+            catch (ArgumentNullException e)
+            {
+                Debug.Log(e.StackTrace);
+            }
+            catch (SocketException e)
+            {
+                Debug.Log(e.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.StackTrace);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.StackTrace);
+        }
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
